a010b1068e51c59a82f55bc1852372defeb1e07b
devji08 <devsharma23101999@gmail.com>
2021-06-05 17:34:54 +0530

    build

---
+++ build/precache-manifest.49960a57f321c04c2136856650948607.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7ec856be2e19ab6b19757acc1822506",
+    "url": "./index.html"
+  },
+  {
+    "revision": "65312bc46bc7606b971f",
+    "url": "./static/css/2.9e3fc118.chunk.css"
+  },
+  {
+    "revision": "b1486ed2ebe4ca2038b3",
+    "url": "./static/css/main.345f640c.chunk.css"
+  },
+  {
+    "revision": "65312bc46bc7606b971f",
+    "url": "./static/js/2.e8d80b8c.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.e8d80b8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1486ed2ebe4ca2038b3",
+    "url": "./static/js/main.09eed30a.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  }
+]);
